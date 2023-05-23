037c5d980a7f4e27ad7c4fd7aefc82ce4cffd31d
agustinrp89 <51332295+agustinrp89@users.noreply.github.com>
2023-05-23 11:30:09 -0300

    push

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -l
+
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
+
+$ git add entrypoint.sh
+$ git update-index --chmod=+x entrypoint.sh
+$ git ls-files --stage entrypoint.sh
